3bb78b45c759a1c9f0746f67a12edb75d22f3274
carl2g <degentilecarl@gmail.com>
2019-02-07 08:17:31 +0100

    independent activation function by layer

--- src/NetLayer.rb
+++ src/NetLayer.rb
@@ -1,10 +1,12 @@
 class NetLayer
 
-	attr_accessor :w, :b
+	attr_accessor :w, :b, :actFunc, :primeActFunc
 
-	def initialize(size_y, size_x,  min = -2.7, max = 2.7)
+	def initialize(size_y, size_x, actFunc = 'sigmoid', min = -2.7, max = 2.7, primeActFunc = actFunc + "Prime")
 		@w = initWeights(size_y, size_x, min, max)
 		@b = initWeights(size_x, 1, min, max)
+		@actFunc = actFunc
+		@primeActFunc = primeActFunc
 	end
 
 	def initWeights(size_y, size_x = size_y, min = -1.0, max = 1.0)
--- src/NeuroNet.rb
+++ src/NeuroNet.rb
@@ -10,28 +10,28 @@ class NeuroNet
 
 	end
 
-	def feedForward(layers, x, actFunc = 'sigmoid')
+	def feedForward(layers, x)
 		act = [x]
 		zs = []
 		layers.each do |l|
 			x = x * l.w + l.b
 			zs.push(x)
-			x = send(actFunc, x)
+			x = send(l.actFunc, x)
 			act.push(x)
 		end
 		return [zs, act]
 	end
 
-	def backPropagation(zs, act, y, layers, lrn = 0.05, lossFunc = 'sigmoidPrime')
+	def backPropagation(zs, act, y, layers, lrn = 0.05)
 		puts "Error: #{err(act.last, y)}"
 		i = layers.size - 1
 		dz = costFunc(act.last, y)
-		dw = [layers[i].w - act[i].transpose * dz.applyOp(:*, lrn * (1.0 / dz.size_y)) ** send(lossFunc, act[i + 1])]
+		dw = [layers[i].w - act[i].transpose * dz.applyOp(:*, lrn * (1.0 / dz.size_y)) ** send(layers[i].primeActFunc, act[i + 1])]
 		db = [layers[i].b - dz.sumAxis]
 
 		(0...layers.size - 1).reverse_each do |i|
 			tmp = dz * layers[i + 1].w.transpose
-			dz = tmp ** send(lossFunc, act[i + 1])
+			dz = tmp ** send(layers[i].primeActFunc, act[i + 1])
 			dw.push(layers[i].w - act[i].transpose * dz.applyOp(:*, lrn * (1.0 / dz.size_y)))
 			db.push(layers[i].b - dz.sumAxis)
 		end
@@ -39,9 +39,9 @@ class NeuroNet
 		return [dw.reverse!, db.reverse!]
 	end
 
-	def train(layers, x, y, lrn = 0.05, actFunc = 'sigmoid', actFuncPrime = actFunc + "Prime")
-		zs, act = feedForward(layers, x, actFunc)
-		ws, bs = backPropagation(zs, act, y, layers, lrn, actFuncPrime)
+	def train(layers, x, y, lrn = 0.05)
+		zs, act = feedForward(layers, x)
+		ws, bs = backPropagation(zs, act, y, layers, lrn)
 		(0...layers.size).each do |i|
 			layers[i].w = ws[i]
 			layers[i].b = bs[i]

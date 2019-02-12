acbbba2062357069e86e7921663e99056e5cdedd
carl2g <degentilecarl@gmail.com>
2019-02-12 11:35:59 +0100

    clean knn

--- src/Knn.rb
+++ src/Knn.rb
@@ -3,22 +3,6 @@ require 'bitmap'
 
 class Knn
 
-	attr_accessor :hashed_data, :size_x, :size_y
-
-	def initialize(csv_file)
-		content 		= CSV.read(csv_file)
-		@hashed_data 	= {}
-		content.shift.each { |key| @hashed_data[key] = [] }
-		@hashed_data.each_with_index do |h, i|
-			key = h.first
-			@hashed_data[key] += content.map do |line|
-				line[i]
-			end
-		end
-		@size_x = @hashed_data.size
-		@size_y = @hashed_data.first.last.size
-	end
-
 	def predict(data, m, c_exp_res, nb = 100)
 		res = {}
 		m.matrix.each_with_index do |line, i|
@@ -36,36 +20,6 @@ class Knn
 		end
 		final_res = final_res.sort_by {|key, val| val }
 		return final_res.last
-	end
-
-	def matrix(with = self.labels)
-		newH = @hashed_data.select { |key, val| with.include?(key) }
-		size_y = @size_y
-		size_x = newH.keys.size
-		arr = Matrix.new(size_y, size_x)
-		(0...size_y).each do |x|
-			newH.each_with_index do |h, y|
-				key, val = h
-				arr[x][y] = val[x].to_f
-			end
-		end
-		return arr
-	end
-
-	def remove(label)
-		@hashed_data.delete(label)
-	end
-
-	def [](key)
-		@hashed_data[key]
-	end
-
-	def []=(key, values)
-		@hashed_data[key] = values
-	end
-
-	def labels
-		@hashed_data.keys
 	end
 
 end

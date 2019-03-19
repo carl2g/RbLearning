9254f3bf7a89f205ff424b219221ea47b3045482
carl2g <degentilecarl@gmail.com>
2019-03-19 02:14:45 +0100

    clean repo

---
+++ lib/RbLearning.rb
@@ -0,0 +1,10 @@
+############ src file ############
+require_relative './RbLearning/Matrix'
+require_relative './RbLearning/ActivFunc'
+require_relative './RbLearning/NeuroNet'
+require_relative './RbLearning/LossFunc'
+require_relative './RbLearning/DataManager'
+require_relative './RbLearning/NetLayer'
+
+############ Lib file ############
+require_relative './RbLearning/lib/MatrixLib'

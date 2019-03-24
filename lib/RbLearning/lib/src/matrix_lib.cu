bfbc79c889e9faa5c06dc487aec8f4b9b3c779f3
carl2g <degentilecarl@gmail.com>
2019-03-24 21:09:45 +0100

    cuda_mult_matrix

--- lib/RbLearning/lib/src/matrix_lib.cu
+++ lib/RbLearning/lib/src/matrix_lib.cu
@@ -12,6 +12,10 @@ __global__ void cuda_dot(const double *m1, const double *m2, int size_y, int siz
 	new_m[(blockIdx.x * size_v) + threadIdx.x] = tmp;
 }
 
+__global__ void cuda_mult(const double *m1, const double *m2, double *new_m) {
+	new_m[blockIdx.x] = m1[blockIdx.x] * m2[blockIdx.x];
+}
+
 extern "C" {
 
 	double *dot(const double *m1, const double *m2, int size_y, int size_v, int size_x) {
@@ -36,6 +40,28 @@ extern "C" {
 		return(new_m);
 	}
 
+	double *mult(const double *m1, const double *m2, int size) {
+		double *new_m 		= (double *)malloc((size) * sizeof(double));
+		double *cuda_new_m 	= NULL;
+		double *cuda_m1 		= NULL;
+		double *cuda_m2 		= NULL;
+
+		cudaMalloc((void**)&cuda_new_m, size * sizeof(double));
+		cudaMalloc((void**)&cuda_m1, size * sizeof(double));
+		cudaMalloc((void**)&cuda_m2, size * sizeof(double));
+
+		cudaMemcpy(cuda_m1, m1, (size) * sizeof(double), cudaMemcpyHostToDevice);
+		cudaMemcpy(cuda_m2, m2, (size) * sizeof(double), cudaMemcpyHostToDevice);
+
+		cuda_mult<<<size, 1>>>(cuda_m1, cuda_m2, cuda_new_m);
+		cudaMemcpy(new_m, cuda_new_m, size * sizeof(double), cudaMemcpyDeviceToHost);
+
+		cudaFree(cuda_new_m);
+		cudaFree(cuda_m1);
+		cudaFree(cuda_m2);
+		return(new_m);
+	}
+
 	double *transpose(const double  *m, int size_y, int size_x)
 	{
 		double *new_m = (double *)malloc(sizeof(double) * (size_x * size_y));
@@ -44,16 +70,6 @@ extern "C" {
 			for (int x = 0; x < size_x; ++x) {
 				new_m[x * size_y + y] = m[y * size_x + x];
 			}
-		}
-		return (new_m);
-	}
-
-	double *mult(const double *m1, const double *m2, int size_y, int size_x) {
-		int size = size_x * size_y;
-		double *new_m = (double *)malloc(sizeof(double) * size);
-
-		for (int i = 0; i < size; ++i) {
-			new_m[i] = m1[i] * m2[i];
 		}
 		return (new_m);
 	}
--- lib/RbLearning/lib/src/matrix_lib.h
+++ lib/RbLearning/lib/src/matrix_lib.h
@@ -4,7 +4,7 @@
 extern "C" {
 	double *dot(const double *m1, const double *m2, int size_y, int size_v, int size_x);
 	double *transpose(const double *m, int size_y, int size_x);
-	double *mult(const double *m1, const double *m2, int size_y, int size_x);
+	double *mult(const double *m1, const double *m2, int size);
 	double *subtract(const double *m1, const double *m2, int size_y, int size_x);
 	double *add(const double *m1, const double *m2, int size_y, int size_x);
 }

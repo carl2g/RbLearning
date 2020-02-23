bb3f4330437a69d64f8ce23bd048972de3e14ccf
carl2g <degentilecarl@gmail.com>
2020-02-23 13:31:06 +0100

    gradint checking for every layers

--- lib/test/GradientCheckingTest.rb
+++ lib/test/GradientCheckingTest.rb
@@ -5,56 +5,72 @@ class GradientCheckingTest < Test::Unit::TestCase
 	EPSYLON = 0.00001
 
 	def test_1
-		train_x = Matrix.set([[4, 6, 7], [6, 8, 10]])
-		train_y = Matrix.set([[10], [12]])
+		train_x = Matrix.set([
+			[4, 6, 7], 
+			[6, 8, 10],
+			[2, 32, 14]
+		])
+		train_y = Matrix.set([
+			[10], 
+			[12],
+			[32]
+		])
 		lrn = 0.01
 
 		layers = [
 			Input.new(train_x.size_x),
 			NetLayer.new(
-				size: 1,
+				size: 64,
 			 	lrn: lrn,
+	 		activFunction: ActivFunc::ReLu
+			),
+			NetLayer.new(
+			size: 1,
+			lrn: lrn,
 	 		activFunction: ActivFunc::ReLu
 			)
 		]
 		net = NeuroNet.new(costFunction: CostFunc::MeanSqrtErr)
 		net.addLayers(layers)
 
-		original_w = layers.last.w.copy
 
-		layers.last.b = layers.last.b.applyOp(:*, 0)
+		layers.each do |l|
+			original_w = l.w.copy
+			l.b = l.b.applyOp(:*, 0)
+			(0...l.w.size_y).each do |j|
+				(0...l.w.size_x).each do |i|
+					
+					l.w = original_w.copy
+					l.w[j, i] -= EPSYLON
+					zs, act = net.feedForward(train_x)
+					a = net.costFunc.func(act.last, train_y)
 
-		(0...layers.last.w.size_x).each do |i|
-			
-			layers.last.w = original_w.copy
-			layers.last.w[0, i] -= EPSYLON
-			zs, act = net.feedForward(train_x)
-			a = net.costFunc.func(act.last, train_y)
+					l.w = original_w.copy
+					l.w[j, i] += EPSYLON
+					zs, act = net.feedForward(train_x)
+					b = net.costFunc.func(act.last, train_y)
 
-			layers.last.w = original_w.copy
-			layers.last.w[0, i] += EPSYLON
-			zs, act = net.feedForward(train_x)
-			b = net.costFunc.func(act.last, train_y)
+					delt = (b - a).applyOp(:*, lrn / (2 * EPSYLON)).sumOrd
+					updated_w = (original_w - delt).applyOp(:round, 6)
+					
+					puts "==========delta w(#{j})(#{i}) ========"
+					delt.printM(5)
 
-			delt = (b - a).applyOp(:*, lrn / (2 * EPSYLON)).sumOrd
-			updated_w = (original_w - delt).applyOp(:round, 6)
-			
-			puts "==========delta========"
-			delt.printM(5)
+					l.w = original_w.copy
+					layers, loss = net.internal_train(train_y, train_x)
+					l.w = l.w.applyOp(:round, 6)
 
-			layers.last.w = original_w.copy
-			layers, loss = net.internal_train(train_y, train_x)
-			layers.last.w = layers.last.w.applyOp(:round, 6)
+					puts "========== W(#{j})(#{i}) after training ========"
+					puts l.w[j, i]
 
-			puts "========== W after training ========"
-			puts layers.last.w[0, i]
-
-			puts "========== Calculated new W ========"
-			puts updated_w[0, i]
-			puts "=" * 80
-			res = updated_w[0, i] == layers.last.w[0, i]
-	    	assert(res, "Derivation is not the same")
-	    end
+					puts "========== Manualy calculated updated W(#{j})(#{i}) ========"
+					puts updated_w[j, i]
+					puts "=" * 80
+					res = updated_w[j, i] == l.w[j, i]
+			    	assert(res, "Derivation is not the same")
+			    end
+			end
+		end
 	end
 
 end
